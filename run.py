78d548ff27c646aeef8e43209bedd98100fab819
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-02-28 18:51:10 +0000

    add function to choose a word from the word_bank

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Start the Game")
-print("Type your Guess and press enter")
+
+import random
+
+
+def game_word():
+    with open("word_bank.txt") as f:
+        chosen_word = f.read().splitlines()
+        return random.choice(chosen_word)
+
+print("Let's play Wordle:")
+print("Type a 5 letter word and hit enter!\n")
