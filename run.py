3c4c1300917a64dc6bfb9c1ed75a5ec1eec31f44
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-02-28 19:25:27 +0000

    add code to check if letters are in the word and message for endgame

--- run.py
+++ run.py
@@ -12,3 +12,19 @@ def game_word():
 
 print("Let's play Wordle:")
 print("Type a 5 letter word and hit enter!\n")
+
+for i <= 5:
+            if guess[i] == word[i]:
+                print(colored(guess[i], 'green'), end="")
+            elif guess[i] in word:
+                print(colored(guess[i], 'yellow'), end="")
+            else:
+                print(colored(guess[i], 'red'), end="")
+        print()
+
+        if guess == word:
+            print(colored(f"Woo! Nice Job! You answered right after {attempt} attempts", 'red'))
+            break
+        elif attempt == 6:
+            print(f"Nice try. The answer for this round was {word}")
+    play_again = input("Ready for Another Round? Type q and press enter to exit.")
