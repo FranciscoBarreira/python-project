26945cc90392528891740619f4280c98e4be85af
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-03-07 16:28:57 +0000

    add error for words with length different than5

--- run.py
+++ run.py
@@ -3,6 +3,8 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 import random
+from termcolor import colored
+
 
 
 def game_word():
@@ -10,21 +12,23 @@ def game_word():
         chosen_word = f.read().splitlines()
         return random.choice(chosen_word)
 
+
 print("Let's play Wordle:")
 print("Type a 5 letter word and hit enter!\n")
 
-for i <= 5:
-            if guess[i] == word[i]:
-                print(colored(guess[i], 'green'), end="")
-            elif guess[i] in word:
-                print(colored(guess[i], 'yellow'), end="")
-            else:
-                print(colored(guess[i], 'red'), end="")
-        print()
+word = game_word().lower()
+for attempt in range(1, 7):
+    guess = input().lower()
+    try:
+        if len(guess) != 5:
+            raise ValueError(
+                f"The word must be 5 letters long, your word was {len(guess)} letters long"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
-        if guess == word:
-            print(colored(f"Woo! Nice Job! You answered right after {attempt} attempts", 'red'))
-            break
-        elif attempt == 6:
-            print(f"Nice try. The answer for this round was {word}")
-    play_again = input("Ready for Another Round? Type q and press enter to exit.")
+    if guess == word:
+        print(colored(f"Woo! Nice Job! You answered right after {attempt} attempts", 'red'))
+        break
+    elif attempt == 6:
+        print(f"Nice try. The answer was {word}")
